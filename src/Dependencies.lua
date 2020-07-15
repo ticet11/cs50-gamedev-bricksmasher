5dd2c8db0dc855a7dbe603d259bd38a0165c7338
Brian Kozub <ticet11@gmail.com>
2020-07-15 14:35:19 -0600

    added new files to dependencies.lua

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -4,8 +4,14 @@ Class = require 'lib/class'
 
 require 'src/constants'
 
+require 'src/Paddle'
+
 require 'src/StateMachine'
+
+require 'src/Util'
 
 require 'src/states/BaseState'
 
 require 'src/states/StartState'
+
+require 'src/states/PlayState'
